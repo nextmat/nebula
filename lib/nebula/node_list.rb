c2f4b297879ce6ce2a48ae1c0ba4b521cb54f05f
Matt Sanders <matt@polycot.com>
2010-06-19 01:57:59 -0500

    NodeList#count should return the number of actual instantiated nodes. NodeList#length should return actual array length.

--- lib/nebula/node_list.rb
+++ lib/nebula/node_list.rb
@@ -2,6 +2,11 @@ module Nebula
 
   class NodeList < Array
     
+    def count
+      self.compact.length
+    end
+    alias_method :size, :count
+    
   end
 
 end
--- test/test_node_list.rb
+++ test/test_node_list.rb
@@ -2,7 +2,33 @@ require File.dirname(__FILE__) + '/helper'
 
 class TestNodeList < Test::Unit::TestCase
 
-  def test_true
+  context "Counting nodes" do
+    setup do
+      @node_list = Nebula::NodeList.new
+      @node_list << Nebula::Node.new(:id => 0)
+    end
+
+    should 'be able to count contained nodes' do
+      assert_equal 1, @node_list.count
+    end
+    
+    context "with non-contiguous nodes" do
+      setup do
+        @node_list[47] = Nebula::Node.new(:id => 47)
+      end
+
+      should 'count existing nodes with #count' do
+        assert_equal 2, @node_list.count
+      end
+      
+      should 'count existing nodes with #size' do
+        assert_equal 2, @node_list.size
+      end
+      
+      should 'return actual array length with #length' do
+        assert_equal 48, @node_list.length
+      end
+    end
   end
 
 end
